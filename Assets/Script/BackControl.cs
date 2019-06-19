e63742392618ef29ecaac203e1e50cbd3c190da6
yoshiyori <m0118278c9@edu.teu.ac.jp>
2019-06-19 17:18:41 +0900

    Update BackControl.cs
    追従プログラムをましなものにした

--- Assets/Script/BackControl.cs
+++ Assets/Script/BackControl.cs
@@ -11,6 +11,7 @@ public class BackControl : MonoBehaviour
     public GameObject Flt;//前の奴
     Vector3 FrontTarget;
     public BoxControl Box;
+    private float vel;
 
 
     public float HP;
@@ -18,15 +19,19 @@ public class BackControl : MonoBehaviour
     void Start()
     {
         FrontTarget = Flt.transform.position;
-        this.HP = 100.0f;
+        this.HP = Box.MAX_HP;
+        vel = Box.MOVE_SPEED;
     }
 
 
 
     void Update()
     {
+        var pos = transform.position;
+        /*
         transform.position += Flt.transform.position - FrontTarget;
         FrontTarget = Flt.transform.position;
+
         if (Box.CurveFlag == 1)
         {
             transform.RotateAround(Flt.transform.position, Vector3.up, 1);
@@ -35,17 +40,49 @@ public class BackControl : MonoBehaviour
         {
             transform.RotateAround(Flt.transform.position, Vector3.up, -1);
         }
+        */
+        this.transform.LookAt(Flt.transform);
+        Vector3 vecFront = transform.forward.normalized;
+        if (Input.GetKey(KeyCode.Z) && Input.GetKey(KeyCode.LeftShift) && vel < Box.MOVE_SPEED)
+        {
+            //pos.x += (BOOST_SPEED - vel) * vecFront.x * Time.deltaTime;
+            //pos.y += (BOOST_SPEED - vel) * vecFront.y * Time.deltaTime;
+            //pos.z += (BOOST_SPEED - vel) * vecFront.z * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.Z) && Input.GetKey(KeyCode.LeftShift))
-        { 
+            pos += (Box.BOOST_SPEED - vel) * vecFront * Time.deltaTime;
             this.HP -= 0.05f;
-
         }
         else if (Input.GetKey(KeyCode.Z))
         {
             this.HP -= 0.01f;
+            //pos.x += (Box.MOVE_SPEED - vel) * vecFront.x * Time.deltaTime;
+            //pos.y += (Box.MOVE_SPEED - vel) * vecFront.y * Time.deltaTime;
+            //pos.z += (Box.MOVE_SPEED - vel) * vecFront.z * Time.deltaTime;
+            pos += (Box.MOVE_SPEED - vel) * vecFront * Time.deltaTime;
+            if (vel > 0)
+            {
+                vel -= 0.05f;
+            }
+            else if (vel < 0)
+            {
+                vel = 0.0f;
+            }
         }
 
+        if (Box.StopFlag == true)
+        {
+            pos.x += (Box.MOVE_SPEED - vel) * vecFront.x * Time.deltaTime;
+            pos.y += (Box.MOVE_SPEED - vel) * vecFront.y * Time.deltaTime;
+            pos.z += (Box.MOVE_SPEED - vel) * vecFront.z * Time.deltaTime;
+            vel += 0.05f;
+            if (vel > Box.MOVE_SPEED)
+            {
+                vel = Box.MOVE_SPEED;
+            }
+        }
+
+
+        transform.position = pos;
     }
 
 
